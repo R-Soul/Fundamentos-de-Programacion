00bab3ba64d54a4b58a240b4ee39c4352b8c29b3
R-Soul <luis.estu.amigo@hotmail.com>
2014-11-18 17:16:13 -0700

    Human Genome

--- Regular Expressions.py
+++ Regular Expressions.py
@@ -1,11 +1,13 @@
 import re
 
 identifiers = open("RF00059_vs_UTR_Todas_sp_unicas","r")
+findhere = open("UTR_Todas_sp_unicas_linea.txt","r")
 extractid = open("results.txt", 'w')
 
 score = re.compile('\d{2}[.]\d{1}')
 sequence = re.compile("[a-zA-Z]{3}[-][a-zA-Z]{1}[^ ]*")
 keys = []
+counter = 0
 
 for line in identifiers:
     score2 = re.findall(score, line)
@@ -15,12 +17,23 @@ for line in identifiers:
             if len(data) > 0:
                 keys.append(data[0])
 
-k = list(set(keys))
-for item in k:
-    extractid.write(item+"\n")
-    
-findthese = open("results.txt", 'r')
-#NEED THE REST
+byeduplicates = list(set(keys))
+
+findhere2 = []
+for item in findhere:
+    findhere2.append(item)
+
+for item in byeduplicates:
+    cond = False
+    for line in findhere2:
+        if cond:
+            counter += 1
+            print(str((counter/len(byeduplicates))*100)[0:4]+"% Done")
+            extractid.write(item + "," + line)
+            cond = False
+            break
+        if item in line:
+            cond = True
 
 identifiers.close()
 extractid.close()
